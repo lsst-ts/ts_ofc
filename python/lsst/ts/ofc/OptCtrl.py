1ac24d39b789c3876f1adfbc1d8a9bd884689203
Te-Wei Tsai <TTsai@lsst.org>
2018-11-02 15:22:42 -0700

    Implement most functions of OptCtrl class.

--- python/lsst/ts/ofc/OptCtrl.py
+++ python/lsst/ts/ofc/OptCtrl.py
@@ -5,6 +5,8 @@ import numpy as np
 from lsst.ts.ofc.Utility import InstName, getMatchFilePath, \
                                 getDirFiles, getSetting
 
+from lsst.ts.ofc.OptStateEsti import OptStateEsti
+
 
 class OptCtrl(object):
 
@@ -242,58 +244,383 @@ class OptCtrl(object):
 
         return authority
 
-    def calcEffGQFWHM(self):
-        pass
+    def getAuthority(self):
+        """Get the authority of subsystems.
+        
+        The penality is considered. The array order is [M2 hexapod,
+        camera hexapod, M1M3 bending mode, M2 bending mode].
+        
+        Returns
+        -------
+        numpy.ndarray
+            Authority of subsystem.
+        """
+        
+        return self.authority
 
-    def getNumOfState0(self):
-        pass
+    def setGain(self, gain):
+        """Set the gain value.
 
-    def getNumOfFieldInQwgt(self):
-        pass
-
-    def setState0(self):
-        pass
-
-    def setState(self):
-        pass
+        Parameters
+        ----------
+        gain : float
+            Gain value in the feedback. It should be in the range of 0 and 1.
+        
+        Raises
+        ------
+        ValueError
+            Gain is not in the range of [0, 1].
+        """
+        
+        if (0 <= gain <= 1):
+            self.gain = gain
+        else:
+            raise ValueError("Gain is not in the range of [0, 1].")
 
     def getGain(self):
-        pass
+        """Get the gain value.
 
-    def setGain(self):
-        pass
+        Returns
+        -------
+        float
+            Gain value in the feedback.
+        """
+        
+        return self.gain
 
-    def getState(self):
-        pass
+    def setState0(self, state0InDof):
+        """Set the state 0 in degree of freedom (DOF).
 
-    def getState0(self):
-        pass
+        Parameters
+        ----------
+        state0InDof : numpy.ndarray or list
+            State 0 in DOF.
+        """
+        
+        self.state0InDof = np.array(state0InDof)
 
-    def getAuthority(self):
+    def setState(self, stateInDof):
+        """Set the state in degree of freedom (DOF).
+
+        Parameters
+        ----------
+        stateInDof : numpy.ndarray or list
+            State in DOF.
+        """
+        
+        self.stateInDof = np.array(stateInDof)
+
+    def getState0(self, dofIdx):
+        """Get the state 0 in degree of freedom (DOF).
+
+        Parameters
+        ----------
+        dofIdx : numpy.ndarray[int] or list[int]
+            Index array of degree of freedom.
+
+        Returns
+        -------
+        numpy.ndarray
+            State 0 in DOF.
+        """
+        
+        return self.state0InDof[dofIdx]
+
+    def getState(self, dofIdx):
+        """Get the state in degree of freedom (DOF).
+
+        Parameters
+        ----------
+        dofIdx : numpy.ndarray[int] or list[int]
+            Index array of degree of freedom.
+
+        Returns
+        -------
+        numpy.ndarray
+            State in DOF.
+        """
+        
+        return self.stateInDof[dofIdx]
+
+    def getNumOfState0(self):
+        """Get the number of element of state 0.
+
+        Returns
+        -------
+        int
+            Number of element of state 0.
+        """
+        
+        return len(self.state0InDof)
+
+    def aggState(self, calcDof, dofIdx):
+        """Aggregate the calculated degree of freedom (DOF) in the state.
+
+        Parameters
+        ----------
+        calcDof : numpy.ndarray
+            Calculated DOF.
+        dofIdx : numpy.ndarray[int] or list[int]
+            Index array of degree of freedom.
+        """
+        
+        addDof = np.zeros(self.getNumOfState0())
+        addDof[dofIdx] = calcDof
+
+        self.stateInDof += addDof
+
+    def getDofFromFile(self, dofFilePath):
+        """Get the degree of freedom (DOF) from file.
+
+        Parameters
+        ----------
+        dofFilePath : str
+            DOF file path.
+        
+        Returns
+        -------
+        numpy.ndarray
+            DOF.
+        """
+        
+        return np.loadtxt(dofFilePath, usecols=1)
+
+    def getGroupDof(self, startIdx, groupLeng, inputDof=None):
+        """Get the degree of freedom (DOF) of specific group based on the
+        start index and length.
+
+        If there is no input DOF, the output will be the aggregated DOF
+        (state - state0). Otherwise, the output is based on the input DOF.
+
+        The default output units are:
+        1. M2 hexapod position (dz in um, dx in um, dy in um, rx in arcsec,
+        ry in arcsec).
+        2. Cam hexapod position (dz in um, dx in um, dy in um, rx in arcsec,
+        ry in arcsec).
+        3. M1M3 20 bending mode in um.
+        4. M2 20 bending mode in um.
+
+        Parameters
+        ----------
+        startIdx : int
+            Start index of group.
+        groupLeng : int
+            Index length of group.
+        inputDof : numpy.ndarray or list, optional
+            Input DOF. (the default is None.)
+
+        Returns
+        -------
+        numpy.ndarray
+            DOF.
+        """
+
+        if (inputDof is None):
+            dof = self.stateInDof - self.state0InDof 
+        else:
+            dof = np.array(inputDof)
+
+        return dof[np.arange(startIdx, startIdx+groupLeng)]
+
+    def setMatF(self, zn3Idx, dofIdx, effWave, senM):
+        """Set the F matrix. 
+
+        F = inv(A.T * C.T * C * A + rho * H).
+
+        Parameters
+        ----------
+        zn3Idx : numpy.ndarray[int] or list[int]
+            Index array of z3 to zn.
+        dofIdx : numpy.ndarray[int] or list[int]
+            Index array of degree of freedom.
+        effWave : float
+            Effective wavelength in um.
+        senM : numpy.ndarray
+            Sensitivity matrix M.
+        """
+
+        matH = self._getMatH(dofIdx)
+
+        qWgt = self._getQwgtFromFile()
+        pssnAlpha = self._getPssnAlphaFromFile()
+        ccMat = self._calcCCmat(pssnAlpha, effWave, zn3Idx)
+        qMat = self._calcQmat(ccMat, senM, qWgt)
+
+        self.matF = self._calcF(qMat, matH)
+
+    def _getQwgtFromFile(self):
+        """Get the weighting ratio of image quality from file. This is
+        used in the Q matrix calculation.
+
+        Returns
+        -------
+        numpy.ndarray
+            Weighting ratio for the iamge quality Q matrix calculation.
+        """
+
+        filePath = os.path.join(self._getInstDir(), self.weightingFileName)
+        qWgt = np.loadtxt(filePath, usecols=1)
+
+        # Do the normalization
+        qWgt = qWgt/np.sum(qWgt)
+
+        return qWgt
+
+    def _getPssnAlphaFromFile(self):
+        """Get the PSSN alpha value from file.
+        
+        PSSN: Normalized point source sensitivity.
+        
+        Returns
+        -------
+        numpy.ndarray
+            PSSN alpha.
+        """
+        
+        filePath = os.path.join(self.configDir, self.pssnAlphaFileName)
+        pssnAlpha = np.loadtxt(filePath, usecols=0)
+
+        return pssnAlpha
+
+    def _getMatH(self, dofIdx):
+        """Get the matrix H used in the control algorithm.
+
+        This matrix has considered the affection of penality on the
+        authority of groups.
+
+        Parameters
+        ----------
+        dofIdx : numpy.ndarray[int] or list[int]
+            Index array of degree of freedom.
+        
+        Returns
+        -------
+        numpy.ndarray
+            Matrix H used in the cost function.
+        """
+        
+        authority = self.authority[dofIdx]
+        matH = np.diag(authority**2)
+
+        return matH
+
+    def _calcCCmat(self, pssnAlpha, effWave, zn3Idx):
+        """Calculate the CC matrix used in matrix Q. It is C.T * C
+        used in Q = A.T * (C.T * C) * A.
+        
+        Cost function: J = x.T * Q * x + rho * u.T * H * u
+        Choose x.T * Q * x = p.T * p
+        p = C * y = C * (A * x)
+        p.T * p = (C * A * x).T * C * A * x 
+                = x.T * (A.T * C.T * C * A) * x = x.T * Q * x
+        CCmat is C.T *C above
+        
+        Parameters
+        ----------
+        pssnAlpha : numpy.ndarray
+            PSSN alpha.
+        effWave : float
+            Effective wavelength in um.
+        zn3Idx : numpy.ndarray[int] or list[int]
+            Index array of z3 to zn.
+        
+        Returns
+        -------
+        numpy.ndarray
+            C.T * C matrix.
+        """
+        
+        ccMat = (2*np.pi/effWave)**2 * pssnAlpha[zn3Idx]
+        ccMat = np.diag(ccMat)
+
+        return ccMat
+
+    def _calcQmat(self, ccMat, senM, qWgt):
+        """Calculate the Q matrix used in the cost function.
+
+        Parameters
+        ----------
+        ccMat : numpy.ndarray
+            C.T * C matrix.
+        senM : numpy.ndarray
+            Sensitivity matrix M.
+        qWgt : numpy.ndarray
+            Weighting ratio for the iamge quality Q matrix calculation.
+
+        Returns
+        -------
+        numpy.ndarray
+            Q matrix used in cost functin.
+        """
+
+        # Qi = A.T * C.T * C * A for each field point 
+        # Final Q := sum_i (wi * Qi)        
+        qMat = 0
+        for aMat, wgt in zip(senM, qWgt):
+            qMat += wgt * aMat.T.dot(ccMat).dot(aMat)
+
+        return qMat
+
+    def _calcF(self, qMat, matH):
+        """Calculate the F matrix. F = inv(A.T * C.T * C * A + rho * H).
+        
+        u = - A.T * C.T * C * A / (A.T * C.T * C * A + rho * H) * x0
+          = - F * (A.T * C.T * C * A) * x0
+        F = inv( A.T * C.T * C * A + rho * H )
+        Q = A.T * C.T * C * A
+        
+        Parameters
+        ----------
+        qMat : numpy.ndarray
+            Q matrix used in cost functin.
+        matH : numpy.ndarray
+            Matrix H used in the cost function.
+        
+        Returns
+        -------
+        numpy.ndarray
+            F matrix.
+        """
+        
+        # Because the unit is rms^2, the square of rho read from
+        # the *.ctrl file is needed.
+        matF = np.linalg.inv(self.penality["Motion"]**2 * matH + qMat)
+
+        return matF
+
+    def getMatF(self):
+        """Get the F matrix.
+
+        F = inv(A.T * C.T * C * A + rho * H).
+
+        Returns
+        -------
+        numpy.ndarray
+            F matrix.
+        """
+
+        return self.matF
+
+    def getNumOfFieldInQwgt(self):
+        """Get the number of field in the image quality weighting ratio.
+
+        Returns
+        -------
+        int
+            Number of field for the image quality weighting ratio.
+        """
+        
+        qWgt = self._getQwgtFromFile()
+
+        return len(qWgt)
+
+    def calcEffGQFWHM(self):
         pass
 
     def getMotRng(self):
         pass
 
-    def getMatF(self):
-        pass
-
-    def getMatF(self):
-        pass
-
-    def setMatF(self):
-        pass
-
     def estiUk(self):
-        pass
-
-    def aggState(self):
-        pass
-
-    def getDofFromFile(self):
-        pass
-
-    def getGroupDof(self):
         pass
 
     def _calcUkRef0(self):
@@ -305,28 +632,10 @@ class OptCtrl(object):
     def _calcUkRefX0(self):
         pass
 
-    def _calcQmat(self):
-        pass
-
     def _calcUk(self):
         pass
 
     def _calcQx(self):
-        pass
-
-    def _calcF(self):
-        pass
-
-    def _calcCCmat(self):
-        pass
-
-    def _getPssnAlpha(self):
-        pass
-
-    def _getQwgtFromFile(self):
-        pass
-
-    def _getMatH(self):
         pass
 
 
@@ -335,4 +644,22 @@ if __name__ == "__main__":
     configDir = "/home/ttsai/Documents/github/ts_tcs_ofcPython/configData"
     optCtrl = OptCtrl()
     optCtrl.config(configDir, instName=InstName.LSST)
-    authority = optCtrl._calcRigidBodyAuth(optCtrl.rigidBodyStrokeFileName)
+    optCtrl.setGain(1)
+
+    # dofIdx = [2,3,4,5]
+    # calcDof = [0.1, 0.2, 0.3, 0.4]
+    # optCtrl.aggState(calcDof, dofIdx)
+    # print(optCtrl.getState(dofIdx))
+
+    dofFilePath = "/home/ttsai/Documents/github/ts_tcs_ofcPython/tests/testData/lsst_pert_iter1.txt"
+    dof = optCtrl.getDofFromFile(dofFilePath)
+
+    optStateEsti = OptStateEsti()
+    optStateEsti.config(configDir, instName=InstName.LSST)
+
+    effWave = 0.5
+    senM = optStateEsti.senM
+    dofIdx = optStateEsti.dofIdx
+    zn3Idx = optStateEsti.zn3Idx
+    optCtrl.setMatF(zn3Idx, dofIdx, effWave, senM)
+    print(optCtrl.getNumOfFieldInQwgt())

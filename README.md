4eea9ca9b90f634d25d3d7e4f3b54df63f97d613
Te-Wei Tsai <TTsai@lsst.org>
2018-11-19 14:58:15 -0700

    Update the README.md.

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
 # Optical Feedback Control (OFC) in Python
 
-*This module is used to calculate the aggregated degree of freedom (DOF) for the hexpods and mirrors. The process contains: (1) estimate the optical state in the basis of DOF and (2) estimate the offset by minimizing the cost function. This is to fulfill the requirement that the scientist wants to change the OFC algorithm in Python script. This module will provide the interface python script to OFC in LabVIEW.*
+*This module is used to calculate the aggregated degree of freedom (DOF) for the hexpods and mirrors. The process contains: (1) estimate the optical state in the basis of DOF, (2) estimate the offset of DOF used in the next iteration/ visit. The baseline algorithm now is to minimize the cost function, and (3) rotate the DOF based on the camera rotation angle.*
+<br/>
+<br/>
+*This repository is to fulfill the requirement that the scientist wants to change the OFC algorithm in Python script. This module will provide the interface python script to OFC in LabVIEW in a latter time.*
+
+## 1. Version History
+
+*Version 1.0*
+<br/>
+*Finish the OFC with the support of algorithm study in Python.*
+
+*Author: Te-Wei Tsai*
+*Date: 11-19-2018*
+
+## 2. Platform
+
+- *python: 3.6.2*
+
+## 3. Needed Package
+
+- *numpy*
+- *scipy*
+- *pytest (optional, test only.)*
+
+## 4. Use of Module
+
+*Setup the OFC environment:*
+<br/>
+`export PYTHONPATH=$PYTHONPATH:$path_to_ts_tcs_ofcPython_python`
+<br/>
+(e.g. `export PYTHONPATH=$PYTHONPATH:/home/ttsai/Documents/github/ts_tcs_ofcPython/python`)
+
+## 5. Content
+
+*This module contains the following classes and functions:*
+
+- **DataShare**: Data share class for the information change used in the algorithms. This class includes the information of indexes of zk and DOF to use.
+- **Decorator**: Decorator interface class to add the new functions or attributes to the DataShare class. This helps the user to get the parameters needed in the new algorithms. 
+- **OptStateEstiDataDecorator**: Optical state estimator data decorator class. This adds the functions/ attributes to DataShare class for the parameters needed in the OptStateEsti class (baseline algorithm).
+- **OptCtrlDataDecorator**: Optimal control data decorator class. This adds the functions/ attributes to DataShare class for the parameters needed in the OptCtrl class (baseline algorithm).
+- **OptStateEstiDefault**: Optical state estimator default class. The abstract function interface is declared in this class. The child class should realize the abstract funtion to estimate the optical state in the basis of DOF.
+- **OptStateEsti**: Optical state estimator class in the baseline algorithm. The optica state is estimated by the pseudo-inverse method.
+- **OptCtrlDefault**: Optimal control default class. The abstract function interface is declared in this class. The child class should realize the abstract funtion to calculate the offset of DOF.
+- **OptCtrl**: Optimal control class in the baseline algorithm. The offset is calculated by minimizing the cost function.
+- **ZTAAC**: Zernike to actuator adjustment calculator class. The high-level class to integrate the DataShare, OptStateEstiDefault, and OptCtrlDefault classes.
+- **CamRot**: Camera rotation class to rotate the calculated DOF offset.
+- **Utility**: Some functions used in this module.
+- **IterDataReader**: Iteration data reader class used in the unit test only. This is just to read the test iteration data.
+
+* Class diagram
+
+## 6. Extension of Algorithm
+
+- **Estimate the optical state**: Inherit from the OptStateEstiDefault class, and realize the estiOptState function. Inherit from the Decorator class if new parameter data is needed in the new algorithm.
+- **Calculate the DOF offset**: Inherit from the OptCtrlDefault class, and realize the estiUkWithoutGain function. Inherit from the Decorator class if new parameter data is needed in the new algorithm.
+
+## 7. Reference of Baseline Algorithm
+
+1. Angeli, George Z. et al., Real time wavefront control system for the Large Synoptic Survey Telescope (LSST), Proc. SPIE 9150, Modeling, Systems Engineering, and Project Management for Astronomy VI, 91500H (2014). [https://doi.org/10.1117/12.2055390]
+2. Angeli, George Z. et al., An integrated modeling framework for the Large Synoptic Survey Telescope (LSST), Proc. SPIE 9911, Modeling, Systems Engineering, and Project Management for Astronomy VI, 991118 (2016). [https://doi.org/10.1117/12.2234078]
+3. Angeli, George Z. and Xin, Bo, Normalized Point Source Sensitivity for LSST, document-17242 (2016). [https://docushare.lsst.org/docushare/dsweb/Get/Document-17242]
+4. [Details of optical state estimation and control algorithms.](https://confluence.lsstcorp.org/display/LTS/Control+Algorithm+in+Optical+Feedback+Control)
+5. [Camera rotation and DOF.](https://confluence.lsstcorp.org/display/LTS/Camera+Rotation+and+Degree+of+Freedom)
--- tests/testIteration.py
+++ tests/testIteration.py
@@ -9,6 +9,7 @@ from lsst.ts.ofc.OptCtrlDataDecorator import OptCtrlDataDecorator
 from lsst.ts.ofc.OptStateEsti import OptStateEsti
 from lsst.ts.ofc.OptCtrl import OptCtrl
 from lsst.ts.ofc.ZTAAC import ZTAAC
+from lsst.ts.ofc.CamRot import CamRot
 from lsst.ts.ofc.IterDataReader import IterDataReader
 
 
@@ -17,54 +18,113 @@ class TestIteration(unittest.TestCase):
 
     def setUp(self):
 
+        # Set up the object of data share class. This contains the
+        # information of indexes of zk and degree of freedom (DOF)
+        # to use.
         dataShare = DataShare()
         configDir = os.path.join(getModulePath(), "configData")
         dataShare.config(configDir, instName=InstName.LSST)
 
+        # Decorate the DataShare object to get the data needed
+        # for the OptStateEsti object.
         optStateEstiData = OptStateEstiDataDecorator(dataShare)
         optStateEstiData.configOptStateEstiData()
 
+        # Decorate the DataShare object to get the data needed
+        # for the OptCtrl object.
         mixedData = OptCtrlDataDecorator(optStateEstiData)
         mixedData.configOptCtrlData(configFileName="optiPSSN_x00.ctrl")
 
+        # Instantiate the objects of OptStateEsti and OptCtrl classes.
         optStateEsti = OptStateEsti()
         optCtrl = OptCtrl()
 
+        # Instantiate the ZTAAC object with the configured objects.
         self.ztaac = ZTAAC(optStateEsti, optCtrl, mixedData)
+
+        # Do the configuration of ZTAAC object.
         self.ztaac.config(filterType=FilterType.REF, defaultGain=0.7,
                           fwhmThresholdInArcsec=0.2)
+
+        # Set the state 0 from file. This is only used in the simulation.
+        # In the real control, the state 0 should come from the subsystem
+        # by SAL (software abstracion layer). But the algorithm for this
+        # needs to be developed.
         self.ztaac.setState0FromFile(state0InDofFileName="state0inDof.txt")
+
+        # Initialize the state to state 0.
         self.ztaac.setStateToState0()
 
+        # Instantiate the camera rotation object.
+        self.camRot = CamRot()
+
+        # Set up the rotation angle.
+        # The angle is zero degree in the test data.
+        self.camRot.setRotAng(0)
+
+        # Read the test iteration data by the IM closed-loop simulation.
         iterDataDir = os.path.join(getModulePath(), "tests", "testData",
                                    "iteration")
         self.iterDataReader = IterDataReader(iterDataDir)
 
     def testIteration(self):
 
+        # Read the wavefront sensor Id in the test data.
+        # It is noted that the sensor Id will be used instead of sensor
+        # name in the SAL communication in the contrl mode.
         sensorIdList = self.iterDataReader.getWfsSensorIdList()
+
+        # Map the sensor Id (e.g. 0, 1, etc.) to sensor name (such as "R22_S11").
         sensorNameList = self.ztaac.mapSensorIdToName(sensorIdList)[0]
 
+        # Map the sensor Id to sensor name for the PSSN data.
+        # PSSN is the normalized point source sensitivity.
+        # In the control mode, DM team will provide the image quality (FWHM)
+        # for each sensor. T&S team will transform the FWHM to PSSN.
+        # FWHM is the full-width at half maximum.
         pssnIdList = self.iterDataReader.getPssnSensorIdList()
         pssnSensorNameList = self.ztaac.mapSensorIdToName(pssnIdList)[0]
 
+        # Run the iteration for five times.
         maxIterNum = 5
         for iterNum in range(0, maxIterNum):
+
+            # Read the PSSN data in the specific iteration.
             pssn = self.iterDataReader.getPssn(iterNum)
+
+            # Decide the gain value based on the PSSN data.
             self.ztaac.setGainByPSSN(pssn, pssnSensorNameList)
 
+            # Read the wavefront error data in the specific iteration. 
             wfErr = self.iterDataReader.getWfsErr(iterNum)
-            uk = self.ztaac.estiUkWithGain(wfErr, sensorNameList)
-            self.ztaac.aggState(uk)
 
+            # Calculate the uk based on the control algorithm.
+            uk = self.ztaac.estiUkWithGain(wfErr, sensorNameList)
+
+            # Consider the camera rotation.
+            rotUk = self.ztaac.rotUk(self.camRot, uk)
+
+            # Aggregate the rotated uk.
+            self.ztaac.aggState(rotUk)
+
+            # Collect the DOF for the comparison
             dof = []
             for dofGroup in DofGroup:
+                # Get the DOF for each group
                 dofOfGroup = self.ztaac.getGroupDof(dofGroup)
                 dof = np.append(dof, dofOfGroup)
+
+            # In the simulation mode, the PhoSim needs the total state
+            # of telescope to do the perturbation. In the control mode,
+            # only the aggregated totatl state (state - state0) is needed.
             dof += self.ztaac.getState0()
 
+            # Read the answer of DOF in the test file. The calculated DOF
+            # is applied to the next iteration/ visit. This is why we read
+            # the data in "iterNum + 1" instead of "iterNum".
             dofAns = self.iterDataReader.getDof(iterNum + 1)
 
+            # Compare the DOF with answer.
             delta = np.sum(np.abs(dof - dofAns))
             self.assertLess(delta, 0.002)
 

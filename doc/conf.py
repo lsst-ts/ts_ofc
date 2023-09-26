9f8954ff6b8e5f69d44caf10d681fb57921c9ede
Chris Suberlak <suberlak@uw.edu>
2023-09-26 13:20:26 -0700

    add noqa to doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -5,18 +5,17 @@ This configuration only affects single-package Sphinx documentation builds.
 
 import os.path
 
-import lsst.ts.ofc
-from documenteer.conf.pipelinespkg import *
+from documenteer.conf.pipelinespkg import *  # noqa
 
 project = "ts_ofc"
-html_theme_options["logotext"] = project
+html_theme_options["logotext"] = project  # noqa
 html_title = project
 html_short_title = project
 doxylink = {}
 
 
 # Support the sphinx extension of plantuml
-extensions.append("sphinxcontrib.plantuml")
+extensions.append("sphinxcontrib.plantuml")  # noqa
 
 # Put the path to plantuml.jar
 plantuml_path = os.path.expanduser("~/plantuml.jar")

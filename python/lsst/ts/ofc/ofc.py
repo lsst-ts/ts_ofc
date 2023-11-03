Fixing rotation angles sign

python/lsst/ts/ofc/ofc.py
@@ -63,8 +63,8 @@ class OFC:
         Instance of `OFCController` class.
     ofc_data : `OFCData`
         OFC data container.
-    pssn_data : `dict`
-        Normalized point source sensitivity data.
+    fwhm_data : `dict`
+        Full Width Half Maximum data.
     state_estimator : `StateEstimator`
         Instance of `StateEstimator`.
 
@@ -81,7 +81,7 @@ class OFC:
         else:
             self.log = log.getChild(type(self).__name__)
 
-        self.pssn_data = dict(sensor_id=None, pssn=None)
+        self.fwhm_data = None
 
         self.ofc_data = ofc_data
 
@@ -104,7 +104,7 @@ class OFC:
     def calculate_corrections(
         self,
         wfe: np.ndarray,
-        field_idx: np.ndarray,
+        sensor_names: list,
         filter_name: str,
         gain: float,
         rotation_angle: float,
@@ -118,8 +118,8 @@ class OFC:
             An array of arrays (e.g. 2-d array) with wavefront erros. Each
             element contains an array of wavefront errors (in um) for a
             particular detector/field.
-        field_idx: `np.ndarray`
-            Array with field indices
+        sensor_names: `list`
+            List of sensor names.
         filter_name : `string`
             Name of the filter used in the observations. This must be a valid
             entry in the `ofc_data.intrinsic_zk` and `ofc_data.eff_wavelength`
@@ -142,10 +142,10 @@ class OFC:
             If size of `wfe` is different than `sensor_names`.
         """
 
-        if len(wfe) != len(field_idx):
+        if len(wfe) != len(sensor_names):
             RuntimeError(
                 f"Number of wavefront errors ({len(wfe)}) must be the same as "
-                f"number of field indexes ({len(field_idx)})."
+                f"number of sensors ({len(sensor_names)})."
             )
         # Set the gain value
         if gain < 0.0:
@@ -154,14 +154,14 @@ class OFC:
             self.ofc_controller.gain = gain
 
         optical_state = self.state_estimator.dof_state(
-            filter_name, wfe, field_idx, rotation_angle
+            filter_name, wfe, sensor_names, rotation_angle
         )
 
         # Calculate the uk based on the control algorithm
-        uk = self.ofc_controller.uk_gain(filter_name, optical_state)
+        uk = self.ofc_controller.uk_gain(filter_name, optical_state, sensor_names)
 
         # Remove degeneracies from correction vector
-        #uk = self.ofc_controller.remove_degeneracies(uk, rcond=self.rcond_degeneracy)
+        uk = self.ofc_controller.remove_degeneracies(uk, rcond=self.rcond_degeneracy)
 
         # Assign the value to the last visit DOF
         self.set_last_visit_dof(uk)
@@ -228,36 +228,17 @@ class OFC:
 
         self.lv_dof = np.zeros_like(self.ofc_controller.dof_state0)
 
-    def set_fwhm_data(self, fwhm, sensor_id) -> None:
+    def set_fwhm_data(self, fwhm) -> None:
         """Set the list of FWHMSensorData of each CCD of camera.
 
         Parameters
         ----------
         fwhm : `np.ndarray`
-            Array of arrays (e.g. 2-d array) which contains the FWHM data.
             Each element contains an array of fwhm (in arcsec) measurements for
-            a  particular sensor.
-        sensor_id : `np.array` of `int`
-            Array with the sensor id.
-
-        Raises
-        ------
-        RuntimeError
-            If size of `fwhm` and `sensor_id` are different.
+            a particular sensor.
         """
 
-        if len(fwhm) != len(sensor_id):
-            raise RuntimeError(
-                f"Size of fwhm ({len(fwhm)}) is different than sensor_id ({len(sensor_id)})."
-            )
-
-        self.pssn_data["sensor_id"] = sensor_id.copy()
-        self.pssn_data["pssn"] = np.zeros(len(fwhm))
-
-        for s_id, fw in enumerate(fwhm):
-            self.pssn_data["pssn"][s_id] = np.average(
-                self.ofc_controller.fwhm_to_pssn(fwhm)
-            )
+        self.fwhm_data = fwhm
 
     def reset(self) -> list[Correction]:
         """Reset the OFC calculation state, which is the aggregated DOF now.
@@ -284,23 +265,24 @@ class OFC:
         return self.get_all_corrections()
 
     def set_pssn_gain(self) -> None:
-        """Set the gain value based on the PSSN, which comes from the FWHM by
+        """Set the gain value based on the FWHM, which comes from the FWHM by
         DM team.
 
         Raises
         ------
         RuntimeError
-            If `pssn_data` is not properly set.
+            If `fwhm_data` is not properly set.
         """
 
-        if self.pssn_data["pssn"] is None or self.pssn_data["sensor_id"] is None:
+        if self.fwhm_data is None:
             raise RuntimeError(
-                "PSSN data not set. Run `set_fwhm_data` with appropriate data."
+                "FWHM data not set. Run `set_fwhm_data` with appropriate data."
             )
 
-        fwhm_gq = self.ofc_controller.effective_fwhm_g4(
-            self.pssn_data["pssn"], self.pssn_data["sensor_id"]
-        )
+        fwhm_gq = np.mean(self.fwhm_data)
+
+        if np.isnan(fwhm_gq) or np.isinf(fwhm_gq):
+            raise ValueError("FWHM values are unphysical.")
 
         if fwhm_gq > self.fwhm_threshold:
             self.ofc_controller.gain = 1.0

python/lsst/ts/ofc/ofc_controller.py
@@ -23,8 +23,8 @@ import logging
 
 import numpy as np
 
-from . import BendModeToForce
-from . import SensitivityMatrix
+from . import BendModeToForce, SensitivityMatrix
+from .utils import get_field_angle
 
 
 class OFCController:
@@ -170,41 +170,6 @@ class OFCController:
         else:
             raise ValueError(f"Gain must be in the range of [0, 1]. Got {value}.")
 
-    def effective_fwhm_g4(self, pssn, field_idx):
-        """Calculate the effective FWHM by Gaussian quadrature.
-
-        FWHM: Full width at half maximum.
-        FWHM = eta * FWHM_{atm} * sqrt(1/PSSN -1).
-        Effective GQFWHM = sum_{i} (w_{i}* FWHM_{i}).
-
-        Parameters
-        ----------
-        pssn : `numpy.ndarray` or `list`
-            Normalized point source sensitivity (PSSN).
-        fieldIdx : `numpy.ndarray` or `list` of `int`
-            Field index array.
-
-        Returns
-        -------
-        `float`
-            Effective FWHM in arcsec by Gaussain quadrature.
-
-        Raises
-        ------
-        ValueError
-            Input values are unphysical.
-        """
-
-        # Normalized image quality weight
-        n_imqw = self.ofc_data.normalized_image_quality_weight[field_idx]
-        fwhm = self.ETA * self.FWHM_ATM * np.sqrt(1.0 / np.array(pssn) - 1.0)
-        fwhm_gq = np.sum(n_imqw * fwhm)
-
-        if np.isnan(fwhm_gq) or np.isinf(fwhm_gq):
-            raise ValueError("Input values are unphysical.")
-
-        return fwhm_gq
-
     def authority(self):
         """Compute the authority of the system.
 
@@ -309,7 +274,7 @@ class OFCController:
 
         return self.calc_uk_x0(mat_f=mat_f, qx=_qx)
 
-    def uk_gain(self, filter_name, dof_state):
+    def uk_gain(self, filter_name, dof_state, sensor_names):
         """Estimate uk in the basis of degree of freedom (DOF) with gain
         compensation.
 
@@ -319,6 +284,8 @@ class OFCController:
             Name of the filter.
         dof_state : `numpy.ndarray`
             Optical state in the basis of DOF.
+        sensor_names : `list` of `string`
+            List of sensor names.
 
         Returns
         -------
@@ -326,9 +293,11 @@ class OFCController:
             Calculated uk in the basis of DOF.
         """
 
-        return self.gain * self.uk(filter_name, dof_state)
+        return self.gain * self.uk(filter_name, dof_state, sensor_names)
 
-    def uk(self, filter_name: str, dof_state: np.ndarray) -> np.ndarray:
+    def uk(
+        self, filter_name: str, dof_state: np.ndarray, sensor_names: list[str]
+    ) -> np.ndarray:
         """Estimate the offset (`uk`) of degree of freedom (DOF) at time `k+1`
         based on the wavefront error (`yk`) at time `k`.
 
@@ -341,6 +310,8 @@ class OFCController:
             Name of the filter.
         dof_state : `numpy.ndarray`
             Optical state in the basis of DOF.
+        sensor_names : `list` of `string`
+            List of sensor names.
 
         Returns
         -------
@@ -377,17 +348,26 @@ class OFCController:
 
         _dof_state = dof_state.reshape(-1, 1)
 
-        n_imqw = self.ofc_data.normalized_image_quality_weight
-
         # Constuct the double zernike sensitivity matrix
         dz_sensitivity_matrix = SensitivityMatrix(self.ofc_data)
 
         # Evaluate sensitivity matrix at sensor positions
-        sensitivity_matrix = dz_sensitivity_matrix.evaluate()
+        # If the instrument is LSST, we will use the Gaussian
+        # Quadrature points to evaluate the sensitivity matrix.
+        # Otherwise, for full array mode instruments,
+        # we will use the sensor positions with equal weights.
+        if self.ofc_data.instrument == "lsst":
+            n_imqw = self.ofc_data.normalized_image_quality_weight
+            field_angles = self.ofc_data.gq_points
+        else:
+            n_imqw = np.ones(len(sensor_names)) / len(sensor_names)
+            field_angles = get_field_angle(sensor_names)
+
+        sensitivity_matrix = dz_sensitivity_matrix.evaluate(field_angles)
 
         # Select sensitivity matrix only at used degrees of freedom
         sensitivity_matrix = sensitivity_matrix[:, self.ofc_data.zn3_idx, :]
-        
+
         # Select sensitivity matrix only at used degrees of freedom
         sensitivity_matrix = sensitivity_matrix[..., self.ofc_data.dof_idx]
 
@@ -415,28 +395,6 @@ class OFCController:
         )
 
         return uk.ravel()
-
-    def fwhm_to_pssn(self, fwhm):
-        """Convert the FWHM data to PSSN.
-
-        Take the array of FWHM values (nominally 1 per CCD) and convert
-        it to PSSN (nominally 1 per CCD).
-
-        Parameters
-        ----------
-        fwhm : `numpy.ndarray[x]`
-            An array of FWHM values with sensor information.
-
-        Returns
-        -------
-        pssn : `numpy.ndarray[y]`
-            An array of PSSN values.
-        """
-
-        denominator = self.ETA * self.FWHM_ATM
-        pssn = 1.0 / ((fwhm / denominator) ** 2 + 1.0)
-
-        return pssn
 
     def remove_degeneracies(self, uk: np.ndarray, rcond: float = 1e-7) -> np.ndarray:
         """Remove degeneracies from the correction vector.

python/lsst/ts/ofc/ofc_data/base_ofc_data.py
@@ -41,6 +41,7 @@ def default_eff_wavelenght():
         "I": 0.754,
         "Z": 0.868,
         "Y": 0.973,
+        "": 0.5,
     }
 
 
@@ -131,9 +132,6 @@ class BaseOFCData:
         +5900 um to -5900 um.
     sen_m_filename_root : `string`
         Filename root string for the sensitivity matrix M.
-    sensor_mapping_filename : `string`
-        Name of the file with the mapping between the sensor name and field
-        index.
     y2_filename : `string`
         Name of the file where `y2_correction` is read from.
     zn3_idx : `np.array` of `bool`
@@ -146,10 +144,8 @@ class BaseOFCData:
         Max number of zernikes used (to be filtered with `zn3_idx`).
     """
 
-    iqw_filename: str = "img_quality_weight.yaml"
+    gq_filename: str = "lsst_gq_points.yaml"
     y2_filename: str = "y2.yaml"
-    sensor_mapping_filename: str = "sensor_name_to_field_idx.yaml"
-    field_angles_filename: str = "field_xy.yaml"
     dof_state0_filename: str = "state0_in_dof.yaml"
     intrinsic_zk_filename_root: str = "intrinsic_zk"
     sen_m_filename_root: str = "sensitivity"

python/lsst/ts/ofc/ofc_data/ofc_data.py
@@ -23,11 +23,9 @@ __all__ = ["OFCData"]
 
 import asyncio
 import logging
-
 from glob import glob
 from pathlib import Path
 
-import galsim
 import numpy as np
 import yaml
 
@@ -64,14 +62,14 @@ class OFCData(BaseOFCData):
         other files when the name is set.
     config_dir : `pathlib.Path`
         Path to the directory storing configuration files.
-    control_strategy : `string`
-        Name of the control strategy.
     dof_idx : `dict` of `string`
         Index of Degree of Freedom (DOF).
     field_idx : `dict` of `string`
         Mapping between sensor name and field index.
-    idx_dof : `dict` of `string`
-        Index of Degree of Freedom (DOF)
+    gq_points: `np.ndarray` of `float`
+        Gaussian Quadrature points for LSST field.
+    image_quality_weight : `np.ndarray` of `float`
+        Image quality weight for the Gaussian Quadrature points.
     intrinsic_zk : `dict` of `string`
         Intrinsic zernike coefficients per band per detector for a specific
         instrument configuration.
@@ -266,62 +264,6 @@ class OFCData(BaseOFCData):
                 raise RuntimeError("Input should be np.ndarray of type bool.")
             self._dof_idx_mask[start_idx:end_idx] = value[comp]
 
-    def get_intrinsic_zk(
-        self,
-        filter_name: str,
-        field_idx: np.ndarray | None = None,
-        rotation_angle: float = 0.0,
-    ) -> np.ndarray:
-        """Return reformated instrisic zernike coefficients.
-
-        Parameters
-        ----------
-        filter_name : `string`
-            Name of the filter to get intrinsic zernike coefficients. Must be
-            in the `intrinsic_zk` dictionary.
-        field_idx : `np.ndarray` or `None`, optional
-            Array with the field indexes to get instrisic data from (default
-            `None`). If not given, return all available data.
-        rotation_angle : `float`, optional
-            Rotation angle in degrees.
-
-        Raises
-        ------
-        RuntimeError :
-            If `filter_name` not valid.
-        """
-        if filter_name not in self.intrinsic_zk:
-            raise RuntimeError(
-                f"Invalid filter name {filter_name}. Must be one of {self.intrinsic_zk.keys()}."
-            )
-
-        if field_idx is None:
-            field_x, field_y = zip(*self.gq_field_angles)
-        else:
-            gq_points = self.gq_field_angles[field_idx, :]
-            field_x, field_y = zip(*gq_points)
-
-        # Convert rotation angle to radians
-        rotation_angle = np.deg2rad(rotation_angle)
-        
-        evaluated_zernikes = np.array(
-            [
-                zk.coef
-                for zk in galsim.zernike.DoubleZernike(
-                    self.intrinsic_zk[filter_name],
-                    # Rubin annuli
-                    uv_inner=self.config["pupil"]["R_inner"],
-                    uv_outer=self.config["pupil"]["R_outer"],
-                    xy_inner=self.config["obscuration"]["R_inner"],
-                    xy_outer=self.config["obscuration"]["R_outer"],
-                ).rotate(theta_uv=rotation_angle)(field_x, field_y)
-            ]
-        )
-
-        evaluated_zernikes *= self.eff_wavelength[filter_name]
-
-        return evaluated_zernikes[:, 4:23]
-
     async def configure_instrument(self, instrument: str) -> None:
         """Configure instrument concurrently.
 
@@ -354,6 +296,9 @@ class OFCData(BaseOFCData):
             If image quality weight file not exists in the configuration
             directory.
         """
+
+        # Set the name of the instrument. Either lsst or comcam
+        camera_type = instrument if instrument != "lsstfam" else "lsst"
 
         self.log.debug("Reading bend mode data.")
 
@@ -390,27 +335,20 @@ class OFCData(BaseOFCData):
             dof_state0 = yaml.safe_load(fp)
 
         # Read image quality weight
-        iqw_path = self.config_dir / "sample_points" / instrument / self.iqw_filename
+        gq_path = self.config_dir / "gq_points" / self.gq_filename
 
-        self.log.debug(f"Configuring image quality weight: {iqw_path}")
+        self.log.debug(f"Configuring image quality weight: {gq_path}")
 
         try:
-            with open(iqw_path) as fp:
+            with open(gq_path) as fp:
                 data = yaml.safe_load(fp)
-                # This data has an "interesting" format. The first column is
-                # the index of the sensor and the second is the weight. It kind
-                # of makes sense to use it this way because it is a detector
-                # index not just the line number. In any case, we must make
-                # sure we respect the index on the first column. This is not
-                # exactly efficient but better than risk users entering data
-                # out-of-order expecting the code to handle it and doesn't
-                image_quality_weight = np.zeros(len(data))
-                index = np.array([k for k in data.keys()], dtype=int)
-                _data = np.array([data[k] for k in data])
-                image_quality_weight[index] = _data
+            # The first two elements of each rwo correspond to the field angle,
+            # the third element is the weight.
+            gq_points = np.array([entry[:2] for entry in data])
+            image_quality_weight = np.array([entry[2] for entry in data])
         except FileNotFoundError:
             raise RuntimeError(
-                f"Could not read image quality file from instrument config directory: {iqw_path!s}. "
+                f"Could not read image quality file from instrument config directory: {gq_path!s}. "
                 "Check your instrument configuration directory integrity."
             )
 
@@ -434,25 +372,16 @@ class OFCData(BaseOFCData):
         )
 
         intrinsic_zk = dict()
-        camera_type = instrument if instrument != "lsstfam" else "lsst"
         intrinsic_zk_path = self.config_dir / "intrinsic_zernikes" / camera_type
 
         for filter_name in self.eff_wavelength.keys():
-            file_name = f"{self.intrinsic_zk_filename_root}_{filter_name.lower()}_31*.yaml"
+            file_name = (
+                f"{self.intrinsic_zk_filename_root}_{filter_name.lower()}_31*.yaml"
+            )
             intrinsic_file = Path(glob(str(intrinsic_zk_path / file_name))[0])
 
             with open(intrinsic_file) as fp:
                 intrinsic_zk[filter_name] = np.array(yaml.safe_load(fp))
-
-        self.log.debug(f"Configuring sensor mapping: {self.sensor_mapping_filename}")
-
-        with open(
-            self.config_dir
-            / "sample_points"
-            / instrument
-            / self.sensor_mapping_filename
-        ) as fp:
-            field_idx = yaml.safe_load(fp)
 
         # Load the double zernikes sensitivity matrix
         senm_file = Path(
@@ -470,12 +399,6 @@ class OFCData(BaseOFCData):
             sen_m = np.array(yaml.safe_load(fp))
 
         with open(
-            self.config_dir / "sample_points" / instrument / self.field_angles_filename
-        ) as fp:
-            gq_field_angles = np.array(yaml.safe_load(fp))
-            gq_field_angles[:, 0] *= -1
-
-        with open(
             self.config_dir / "configurations" / f"{camera_type}.yaml"
         ) as yaml_file:
             config = yaml.safe_load(yaml_file)
@@ -483,16 +406,14 @@ class OFCData(BaseOFCData):
         self.log.debug(f"done {instrument}")
         # Now all data was read successfully, time to set it up.
         self.config = config
-        self.image_quality_weight = image_quality_weight
+        self.gq_points = gq_points
         self.normalized_image_quality_weight = image_quality_weight / np.sum(
             image_quality_weight
         )
         self.dof_state0 = dof_state0
         self.y2_correction = y2_correction
         self.intrinsic_zk = intrinsic_zk
-        self.field_idx = field_idx
         self.sensitivity_matrix = sen_m
-        self.gq_field_angles = gq_field_angles
         self.start_task.set_result(instrument)
 
     async def __aenter__(self):

python/lsst/ts/ofc/sensitivity_matrix.py
@@ -21,8 +21,8 @@
 
 __all__ = ["SensitivityMatrix"]
 
-import numpy as np
 import galsim
+import numpy as np
 
 from .ofc_data import OFCData
 
@@ -41,8 +41,8 @@ class SensitivityMatrix:
 
     def evaluate(
         self,
+        field_angles: list,
         rotation_angle: float = 0.0,
-        field_idx: None | np.ndarray = None,
     ) -> np.ndarray:
         """Evaluate the sensitivity matrix for a given rotation angle.
 
@@ -50,9 +50,9 @@ class SensitivityMatrix:
         ----------
         rotation_angle : `float`
             Rotation angle in degrees.
-        field_idx : np.ndarray or None, optional
-            Array of field indices. If None, uses all the Gaussian
-            Quadrature points are used.
+        field_angles : `list` [`tuple` [`float`, `float`]]
+            List of tuples field angles in degrees.
+            [(field_x, field_y)]
 
         Returns
         -------
@@ -60,15 +60,8 @@ class SensitivityMatrix:
             Sensitivity matrix for the given rotation angle in degree.
         """
 
-        # Obtain the field indices for the sensors
-        # If sensor_names is None get sensitivity matrix at all GQ points
-        gq_points = (
-            self.ofc_data.gq_field_angles[field_idx, :]
-            if field_idx is not None
-            else self.ofc_data.gq_field_angles
-        )
-        # Get the field angles for the sensors
-        field_x, field_y = zip(*gq_points)
+        # Get the field angles
+        field_x, field_y = zip(*field_angles)
 
         # Convert rotation angle to radians
         rotation_angle = np.deg2rad(rotation_angle)

python/lsst/ts/ofc/state_estimator.py
@@ -25,8 +25,10 @@ import logging
 
 import galsim
 import numpy as np
+
 from . import SensitivityMatrix
 from .ofc_data import OFCData
+from .utils import get_field_angle, intrinsic_zernikes
 
 
 class StateEstimator:
@@ -98,8 +100,13 @@ class StateEstimator:
         # Constuct the double zernike sensitivity matrix
         dz_sensitivity_matrix = SensitivityMatrix(self.ofc_data)
 
+        # Get the field angles for the sensors
+        field_angles = get_field_angle(self.sensor_names)
+
         # Evaluate sensitivity matrix at sensor positions
-        sensitivity_matrix = dz_sensitivity_matrix.evaluate(rotation_angle, field_idx)
+        sensitivity_matrix = dz_sensitivity_matrix.evaluate(
+            field_angles, rotation_angle
+        )
 
         # Select sensitivity matrix only at used degrees of freedom
         sensitivity_matrix = sensitivity_matrix[:, self.ofc_data.zn3_idx, :]
@@ -144,7 +151,7 @@ class StateEstimator:
         # deviation currently set to zero.
         y = (
             wfe[:, self.ofc_data.zn3_idx]
-            - self.ofc_data.get_intrinsic_zk(filter_name, field_idx, rotation_angle)[
+            - intrinsic_zernikes(filter_name, field_angles, rotation_angle)[
                 :, self.ofc_data.zn3_idx
             ]
             - self.ofc_data.y2_correction[np.ix_(field_idx, self.ofc_data.zn3_idx)]
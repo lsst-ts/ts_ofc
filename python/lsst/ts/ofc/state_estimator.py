Fix sign in rotation_angle

python/lsst/ts/ofc/state_estimator.py
@@ -104,7 +104,7 @@ class StateEstimator:
         field_angles = [self.ofc_data.sample_points[sensor] for sensor in sensor_names]
         field_x, field_y = zip(*field_angles)
 
-        rotation_angle_rad = np.deg2rad(rotation_angle)
+        rotation_angle_rad = np.deg2rad(-rotation_angle)
         rot_mat = np.array(
             [
                 [np.cos(rotation_angle_rad), -np.sin(rotation_angle_rad)],

python/lsst/ts/ofc/utils/ofc_data_helpers.py
@@ -60,7 +60,7 @@ def get_intrinsic_zernikes(
     field_x, field_y = zip(*field_angles)
 
     # Convert rotation angle to radians
-    rotation_angle_rad = np.deg2rad(rotation_angle)
+    rotation_angle_rad = np.deg2rad(-rotation_angle)
     rot_mat = np.array(
         [
             [np.cos(rotation_angle_rad), -np.sin(rotation_angle_rad)],